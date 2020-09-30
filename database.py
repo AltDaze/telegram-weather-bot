94c1bf3dd44ca3b48896e7231d8650d114d1cfde
Alternative Dazemoda <46351186+AltDaze@users.noreply.github.com>
2020-09-30 15:17:19 +0300

    Update database.py
    Got rid of copy-paste. Wrote a database connection decorator.

--- database.py
+++ database.py
@@ -1,47 +1,64 @@
 import sqlite3
 
 class Database():
-
-    # def addToDB(id):
-    # with sqlite3.connect('db.db') as conn:
-    #     cur = conn.cursor()
-    #     cur.execute(f'SELECT COUNT(`id`) FROM `tgbot` WHERE `user_id` = {id}')
-
-    #     if cur.fetchone()[0] == 0:
-    #         cur.execute(f'INSERT INTO `tgbot` (`user_id`) VALUES ({id})')
-    #         conn.commit()
-            
     def DatabaseConnect(func):
-        def Connect():
-            conn = sqlite3.connect('users.db')
-            cur = conn.cursor()
-            return func()
-            conn.commit()
-            cur.close()
-        return Connect()
+        def Connect(*args):
+            with sqlite3.connect('users.db') as conn:
+                cursor = conn.cursor()
+                function = func(*args, cursor)
+                conn.commit()
+                return function
+        return Connect
 
 
-    def NewUser(user_id, lang):
-        conn = sqlite3.connect('users.db')
-        cur = conn.cursor()
+    @DatabaseConnect
+    def NewUser(user_id, lang, cur):
         cur.execute("INSERT INTO users (user_id, language) VALUES (%s, '%s')" % (user_id, lang))
-        conn.commit()
-        cur.close()
 
 
-    def SelectAll():
-        conn = sqlite3.connect('users.db')
-        cur = conn.cursor()
+    @DatabaseConnect
+    def SelectAll(cur):
         cur.execute("SELECT * FROM users")
-        result = ''
-        rows = cur.fetchall()
-        for x in rows:
-            result += str(x)
-        result = result.replace(")", '\n')
-        result = result.replace("(", "")
-        cur.close()
+        result = [row for row in cur.fetchall()]
+        replaces = {
+            "),": "\n",
+            ")": "",
+            "(": "",
+            "[": "",
+            "]": ""}
+        for i, j in replaces.items(): result = result.replace(i, j)
         return result
 
+
+
+    @DatabaseConnect
+    def Language(user_id, cur):
+        cur.execute("SELECT language FROM users WHERE user_id = %s" % user_id)
+        lang = cur.fetchone()
+        return lang[0]
+
+
+    @DatabaseConnect
+    def City(user_id, cur):
+        cur.execute("SELECT city FROM users WHERE user_id = %s" % user_id)
+        city = cur.fetchone()
+        return city[0]
+
+
+    @DatabaseConnect
+    def DeleteUser(user_id, cur):
+        cur.execute("DELETE FROM users WHERE user_id = %s" % user_id)
+
+
+    @DatabaseConnect
+    def UpdateLanguage(lang, user_id, cur):
+        cur.execute("UPDATE users SET LANGUAGE = '%s' WHERE USER_ID = %s" % (lang, user_id))
+
+
+    @DatabaseConnect
+    def UpdateCity(city, user_id, cur):
+        cur.execute("UPDATE users SET CITY = '%s' WHERE USER_ID = %s" % (city, user_id))
+    
 
     # NOT WORK
     def Recreate(self):
@@ -49,43 +66,3 @@ class Database():
         cur = conn.cursor()
         cur.execute('DROP TABLE users')
         cur.execute('create table users (ID INTEGER PRIMARY KEY AUTOINCREMENT, USER_ID int, CITY text, LANGUAGE text)')
-
-
-    def Language(user_id):
-        conn = sqlite3.connect('users.db')
-        cur = conn.cursor()
-        cur.execute("SELECT language FROM users WHERE user_id = %s" % user_id)
-        lang = cur.fetchone()
-        return lang[0]
-
-
-    def City(user_id):
-        conn = sqlite3.connect('users.db')
-        cur = conn.cursor()
-        cur.execute("SELECT city FROM users WHERE user_id = %s" % user_id)
-        city = cur.fetchone()
-        return city[0]
-
-
-    def DeleteUser(user_id):
-        conn = sqlite3.connect('users.db')
-        cur = conn.cursor()
-        cur.execute("DELETE FROM users WHERE user_id = %s" % user_id)
-        conn.commit()
-        cur.close()
-
-
-    def UpdateLanguage(lang, user_id):
-        conn = sqlite3.connect('users.db')
-        cur = conn.cursor()
-        cur.execute("UPDATE users SET LANGUAGE = '%s' WHERE USER_ID = %s" % (lang, user_id))
-        conn.commit()
-        cur.close()
-
-
-    def UpdateCity(city, id):
-        conn = sqlite3.connect('users.db')
-        cur = conn.cursor()
-        cur.execute("UPDATE users SET CITY = '%s' WHERE USER_ID = %s" % (city, user_id))
-        conn.commit()
-        cur.close()
